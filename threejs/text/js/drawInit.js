7f1c67d6df98ea0ae593796ec88697c3dcc16a26
Dmitry Vakarev <dmitryvakarev@gmail.com>
2018-10-15 14:27:19 +0700

    refactoring

--- threejs/text/js/drawInit.js
+++ threejs/text/js/drawInit.js
@@ -2,21 +2,27 @@ import { scene } from './scene.js';
 import { group } from './group.js';
 import { materials } from './materials.js';
 
-// converter http://gero3.github.io/facetype.js/
+// font converter http://gero3.github.io/facetype.js/
 const fontLoad = async () => {
   return new Promise((resolve) => {
-    // helvetiker, optimer, gentilis, droid sans, droid seri;
-    // normal bold;
-    // const url = 'fonts/optimer_bold.typeface.json';
-    // const url = 'fonts/ubuntu_regular.typeface.json';
-    // const url = 'fonts/ubuntu_bold.typeface.json';
-    // const url = 'fonts/mali_regular.typeface.json';
-    // const url = 'fonts/h&b_regular.typeface.json';
-    // const url = 'fonts/habana_regular.typeface.json';
-    const url = 'fonts/Happy brown cat_Regular.json';
-    // const url = 'fonts/Happy brown cat shadow_Regular.json';
+    const font = {
+      depth: 10,
+      size: 50,
+      bold: false,
+    };
+    const url = [
+      // 'fonts/optimer_bold.typeface.json',
+      // 'fonts/ubuntu_regular.typeface.json',
+      // 'fonts/ubuntu_bold.typeface.json',
+      // 'fonts/mali_regular.typeface.json',
+      // 'fonts/h&b_regular.typeface.json',
+      // 'fonts/habana_regular.typeface.json',
+      // 'fonts/Happy brown cat_Regular.json',
+      'fonts/Happy brown cat shadow_Regular.json'
+    ][0];
     new THREE.FontLoader().load(url, function (response) {
-      resolve(response);
+      font.data = response;
+      resolve(font);
     });
   });
 };
@@ -26,17 +32,14 @@ init();
 async function init() {
   const font = await fontLoad();
 
-  const height = 10; // depth for text
-  const size = 50;
-
   const textGeo = new THREE.TextGeometry('ALLMAX', {
-    font: font,
-    size: size,
-    height: height,
+    font: font.data,
+    size: font.size,
+    height: font.depth,
     curveSegments: 4,
     bevelThickness: 2,
     bevelSize: 1.5,
-    bevelEnabled: false, // bold
+    bevelEnabled: font.bold,
   });
 
   textGeo.computeBoundingBox();
@@ -44,33 +47,32 @@ async function init() {
 
   // "fix" side normals by removing z-component of normals for side faces
   // (this doesn't work well for beveled geometry as then we lose nice curvature around z-axis)
-  // if (true) {
+  if (font.bold) {
+    let triangleAreaHeuristics = 0.2 * (font.depth * font.size);
 
-  let triangleAreaHeuristics = 0.2 * (height * size);
+    for (let i = 0; i < textGeo.faces.length; i++) {
+      let face = textGeo.faces[i];
 
-  for (let i = 0; i < textGeo.faces.length; i++) {
-    let face = textGeo.faces[i];
+      if (face.materialIndex === 1) {
 
-    if (face.materialIndex === 1) {
+        for (let j = 0; j < face.vertexNormals.length; j++) {
+          face.vertexNormals[j].z = 0;
+          face.vertexNormals[j].normalize();
+        }
 
-      for (let j = 0; j < face.vertexNormals.length; j++) {
-        face.vertexNormals[j].z = 0;
-        face.vertexNormals[j].normalize();
+        const va = textGeo.vertices[face.a];
+        const vb = textGeo.vertices[face.b];
+        const vc = textGeo.vertices[face.c];
+        const s = THREE.GeometryUtils.triangleArea(va, vb, vc);
+
+        if (s > triangleAreaHeuristics) {
+          for (let j = 0; j < face.vertexNormals.length; j++) {
+            face.vertexNormals[j].copy(face.normal);
+          }
+        }
       }
-
-      const va = textGeo.vertices[face.a];
-      const vb = textGeo.vertices[face.b];
-      const vc = textGeo.vertices[face.c];
-      const s = THREE.GeometryUtils.triangleArea(va, vb, vc);
-
-      // if (s > triangleAreaHeuristics) {
-      for (let j = 0; j < face.vertexNormals.length; j++) {
-        face.vertexNormals[j].copy(face.normal);
-      }
-      // }
     }
   }
-  // }
 
   const centerOffset = -0.5 * (textGeo.boundingBox.max.x - textGeo.boundingBox.min.x);
   const textGeoBuffer = new THREE.BufferGeometry().fromGeometry(textGeo);
@@ -83,18 +85,4 @@ async function init() {
   mainMesh.rotation.x = 0;
   mainMesh.rotation.y = 0;
   group.add(mainMesh);
-
-  // const mirror = true;
-  // if (mirror) {
-  //   const mirrorMesh = new THREE.Mesh(textGeoBuffer, materials);
-
-  //   mirrorMesh.position.x = centerOffset;
-  //   mirrorMesh.position.y = -30;
-  //   mirrorMesh.position.z = height;
-
-  //   mirrorMesh.rotation.x = Math.PI;
-  //   mirrorMesh.rotation.y = Math.PI * 2;
-
-  //   group.add(mirrorMesh);
-  // }
 }
--- threejs/text/js/renderer.js
+++ threejs/text/js/renderer.js
@@ -2,3 +2,4 @@
 export const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
+// renderer.setSize(400, 200);
